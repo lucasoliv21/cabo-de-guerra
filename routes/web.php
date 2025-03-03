e3a0af66bbbc7a424f420d8083bba8089cd9068e
LucasPopan <lucascodantas@hotmail.com>
2025-03-03 15:43:54 -0300

    setando rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VoteController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/lobby', [VoteController::class, 'lobby']);
+
+// Aplica um rate limit para danadinhos nao roubar na nossa votação
+Route::post('/vote/{option}', [VoteController::class, 'vote'])
+    ->middleware('throttle:100,1');
